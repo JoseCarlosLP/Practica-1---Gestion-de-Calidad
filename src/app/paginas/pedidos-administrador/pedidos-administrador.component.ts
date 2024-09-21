16008a39fd83ca91ea688f4e1e57ebb0766fb8e0
unknown <isabqm121@gmail.com>
2024-09-21 18:04:24 -0400

    Remove this unused import of OnInit in pedidos-administrador.component.ts solved

--- src/app/paginas/pedidos-administrador/pedidos-administrador.component.ts
+++ src/app/paginas/pedidos-administrador/pedidos-administrador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { NegociosService } from 'src/app/servicios/negocios.service';
