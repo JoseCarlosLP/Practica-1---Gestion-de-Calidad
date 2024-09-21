4c7047a9993aa3d5e5333966d39a29554ecc5252
unknown <isabqm121@gmail.com>
2024-09-21 19:33:54 -0400

    Issue Fixed: Replace this Number wrapper object with primitive type number in header-administrador.component.ts

--- src/app/elementos/header-administrador/header-administrador.component.ts
+++ src/app/elementos/header-administrador/header-administrador.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-administrador.component.css']
 })
 export class HeaderAdministradorComponent implements OnInit{
-  id: Number = 0;
+  id: number = 0;
   ngOnInit(): void {
     this.id = Number(localStorage.getItem("idNeg"));
     console.log("ID DEL NEGOCIO EN EL HEADER: " + this.id)
