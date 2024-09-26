854cf053851a032a95981089ad18974d57bd4599
Adriel Mounzón <adriel.mounzon@ucb.edu.bo>
2024-09-26 18:03:34 -0400

    Se corrige el método obsoleto subscribe del negocios.component.ts

--- src/app/paginas/negocios/negocios.component.ts
+++ src/app/paginas/negocios/negocios.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NegociosService } from 'src/app/servicios/negocios.service';
 import { Router } from '@angular/router';
 import { CarritoService } from 'src/app/servicios/carrito.service';
+import { catchError, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 
 @Component({
@@ -15,16 +17,18 @@ export class NegociosComponent implements OnInit{
   constructor(private router:Router, private negociosService: NegociosService,private carritoService:CarritoService){}
 
   ngOnInit(): void {
-    this.negociosService.obtenerNegocios().subscribe(
-      data => this.datos = data,
-      error => {
-        if (error.error.error == 'Token expirado') {
-          alert("Su sesion ha expirado, inicie sesion nuevamente")
+    this.negociosService.obtenerNegocios().pipe(
+      tap(data => this.datos = data),
+      catchError(error => {
+        if (error.error.error === 'Token expirado') {
+          alert("Su sesión ha expirado, inicie sesión nuevamente");
           this.router.navigate(['/IniciarSesion']);
         }
-      },
-      () => console.log("Fin de los negocios")
+        return of(null);
+      }),
+      tap(() => console.log("Fin de los negocios"))
     )
+    .subscribe();
     this.carritoService.vaciarCarrito();
   }
 }
